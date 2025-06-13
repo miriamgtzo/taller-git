848b7e0a90f05a20bf6cefc47dfa69b5a4423572
Miriam Gutierrez <miriamgtzofc@gmail.com>
2025-06-12 19:08:46 -0600

    Añadir actions

--- hola.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("¡Hola Mundo!, como estas?\n");
-    return 0;
-}
--- hola_mundo.c
+++ hola_mundo.c
@@ -1,11 +1,16 @@
-//Autor: Miriam
-
 #include <stdio.h>
-#include"funciones.c"
 
-
+int suma (int n) {
+    if (n == 0) return 0;
+    return n + suma (n-1);
+}
 
 int main() {
     printf("¡Hola Mundo!, como estas?\n");
-   
+    return 0;
+}
+
+int factorial(int n ){
+    if (n == 0) return 1 ;
+    return n * factorial(n-1);
 }
